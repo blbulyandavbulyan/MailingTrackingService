cb665d67fcc6a6ebcb21ad0ec146167b0bc0eff8
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-17 09:17:17 +0400

    initial commit

---
+++ src/main/java/com/blbulyandavbulyan/PackTrackingService/PackTrackingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.blbulyandavbulyan.PackTrackingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackTrackingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackTrackingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blbulyandavbulyan/PackTrackingService/PackTrackingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blbulyandavbulyan.PackTrackingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackTrackingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
