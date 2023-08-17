5869c82e584181b8842dbbd67fecac68df1ae5ae
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-17 20:50:24 +0400

    added @Query annotation for updateStatusById

--- src/main/java/com/blbulyandavbulyan/packtrackingservice/repositories/MailingRepository.java
+++ src/main/java/com/blbulyandavbulyan/packtrackingservice/repositories/MailingRepository.java
@@ -3,10 +3,13 @@ package com.blbulyandavbulyan.packtrackingservice.repositories;
 import com.blbulyandavbulyan.packtrackingservice.entities.Mailing;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface MailingRepository extends JpaRepository<Mailing, Long> {
     @Transactional
     @Modifying
-    void updateStatusById(Long mailingId, Mailing.Status status);
+    @Query("UPDATE Mailing m SET m.status = :status WHERE m.mailingId = :mailingId")
+    void updateStatusById(@Param("mailingId") Long mailingId, @Param("status") Mailing.Status status);
 }
