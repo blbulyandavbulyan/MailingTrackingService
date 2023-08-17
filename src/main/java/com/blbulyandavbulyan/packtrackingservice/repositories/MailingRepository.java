20e86d71195597d854b13f0a210a6b0ade694ae9
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-17 17:09:30 +0400

    added updateStatusById in MailingRepository

--- src/main/java/com/blbulyandavbulyan/packtrackingservice/repositories/MailingRepository.java
+++ src/main/java/com/blbulyandavbulyan/packtrackingservice/repositories/MailingRepository.java
@@ -2,6 +2,11 @@ package com.blbulyandavbulyan.packtrackingservice.repositories;
 
 import com.blbulyandavbulyan.packtrackingservice.entities.Mailing;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface MailingRepository extends JpaRepository<Mailing, Long> {
+    @Transactional
+    @Modifying
+    void updateStatusById(Long mailingId, Mailing.Status status);
 }
