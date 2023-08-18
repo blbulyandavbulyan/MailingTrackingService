398e601bd962e32e861e7d4c987f8f4f489f4089
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-18 19:34:29 +0400

    renamed id field

--- src/main/java/com/blbulyandavbulyan/packtrackingservice/dtos/MailingDTO.java
+++ src/main/java/com/blbulyandavbulyan/packtrackingservice/dtos/MailingDTO.java
@@ -2,5 +2,5 @@ package com.blbulyandavbulyan.packtrackingservice.dtos;
 
 import com.blbulyandavbulyan.packtrackingservice.entities.Mailing;
 
-public record MailingDTO(Long mailingId, Mailing.Type type, ReceiverDTO receiver) {
+public record MailingDTO(Long id, Mailing.Type type, ReceiverDTO receiver) {
 }
