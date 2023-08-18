99daab6c93cfb038802149518073c9be9d1f314e
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-18 19:36:26 +0400

    removed receiverId from MovementDTO

--- src/main/java/com/blbulyandavbulyan/packtrackingservice/dtos/MovementDTO.java
+++ src/main/java/com/blbulyandavbulyan/packtrackingservice/dtos/MovementDTO.java
@@ -2,5 +2,5 @@ package com.blbulyandavbulyan.packtrackingservice.dtos;
 
 import java.time.ZonedDateTime;
 
-public record MovementDTO(Long movementId, Long receiverId, Long mailingId, ZonedDateTime arrivalDateTime, ZonedDateTime departureDateTime) {
+public record MovementDTO(Long movementId, Long mailingId, ZonedDateTime arrivalDateTime, ZonedDateTime departureDateTime) {
 }
