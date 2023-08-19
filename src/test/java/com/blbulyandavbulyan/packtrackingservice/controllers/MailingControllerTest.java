1db2e20491d8580403df54140d497fb890e87dc9
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-19 14:56:01 +0400

    added documentation for path parameter in testGettingInfoAboutMailing

--- src/test/java/com/blbulyandavbulyan/packtrackingservice/controllers/MailingControllerTest.java
+++ src/test/java/com/blbulyandavbulyan/packtrackingservice/controllers/MailingControllerTest.java
@@ -29,6 +29,8 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.docu
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -82,18 +84,26 @@ public class MailingControllerTest {
         Mockito.when(mailingService.getInfo(1L)).thenReturn(mailingInfoDTO);
         mockMvc.perform(get("/api/v1/mailings/{id}", mailingInfoDTO.mailingId()))
                 .andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andDo(document("Getting info about mailing", responseFields(
-                        fieldWithPath("mailingId").type(JsonFieldType.NUMBER).description("The identifier of the mailing of this movement"),
-                        mailingTypeField,
-                        fieldWithPath("status").type(JsonFieldType.STRING).description("The status of the mailing").attributes(
-                                key("constraints").value("Must be one of: ON_THE_WAY, IN_THE_DESTINATION, DELIVERED")
-                        ),
-                        fieldWithPath("movements").type(JsonFieldType.ARRAY).description("The movements of the mailing"),
-                        fieldWithPath("movements[].movementId").type(JsonFieldType.NUMBER).description("The id of the movement"),
-                        fieldWithPath("movements[].mailingId").type(JsonFieldType.NUMBER).description("The id of the mailing"),
-                        fieldWithPath("movements[].arrivalDateTime").type(JsonFieldType.STRING).description("The arrival time of the mailing"),
-                        fieldWithPath("movements[].departureDateTime").type(JsonFieldType.STRING).optional().description("The departure time of the mailing")
-                )));
+                .andDo(
+                        document(
+                                "Getting info about mailing",
+                                responseFields(
+                                        fieldWithPath("mailingId").type(JsonFieldType.NUMBER).description("The identifier of the mailing of this movement"),
+                                        mailingTypeField,
+                                        fieldWithPath("status").type(JsonFieldType.STRING).description("The status of the mailing").attributes(
+                                                key("constraints").value("Must be one of: ON_THE_WAY, IN_THE_DESTINATION, DELIVERED")
+                                        ),
+                                        fieldWithPath("movements").type(JsonFieldType.ARRAY).description("The movements of the mailing"),
+                                        fieldWithPath("movements[].movementId").type(JsonFieldType.NUMBER).description("The id of the movement"),
+                                        fieldWithPath("movements[].mailingId").type(JsonFieldType.NUMBER).description("The id of the mailing"),
+                                        fieldWithPath("movements[].arrivalDateTime").type(JsonFieldType.STRING).description("The arrival time of the mailing"),
+                                        fieldWithPath("movements[].departureDateTime").type(JsonFieldType.STRING).optional().description("The departure time of the mailing")
+                                ),
+                                pathParameters(
+                                        parameterWithName("id").description("The id of the mailing, which info will be got")
+                                )
+                        )
+                );
         Mockito.verify(mailingService, Mockito.times(1)).getInfo(mailingInfoDTO.mailingId());
     }
 
