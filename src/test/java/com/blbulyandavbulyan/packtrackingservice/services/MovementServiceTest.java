82740acef744df1629ff1320ea951d11f0bca914
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-21 19:33:29 +0400

    added test createMovementWhenPostalOfficeDoesNotExist

--- src/test/java/com/blbulyandavbulyan/packtrackingservice/services/MovementServiceTest.java
+++ src/test/java/com/blbulyandavbulyan/packtrackingservice/services/MovementServiceTest.java
@@ -1,8 +1,11 @@
 package com.blbulyandavbulyan.packtrackingservice.services;
 
 import com.blbulyandavbulyan.packtrackingservice.entities.Mailing;
+import com.blbulyandavbulyan.packtrackingservice.entities.PostalOffice;
+import com.blbulyandavbulyan.packtrackingservice.entities.Receiver;
 import com.blbulyandavbulyan.packtrackingservice.exceptions.MailingAlreadyDeliveredException;
 import com.blbulyandavbulyan.packtrackingservice.exceptions.MailingNotFoundException;
+import com.blbulyandavbulyan.packtrackingservice.exceptions.PostalOfficeNotFoundException;
 import com.blbulyandavbulyan.packtrackingservice.repositories.MovementRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,12 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -33,14 +31,16 @@ public class MovementServiceTest {
     private MailingService mailingService;
     @Test
     @DisplayName("create movement when mailing is already delivered")
-    public void createMovementWhenEverythingOk(){
+    public void createMovementWhenMailingAlreadyDelivered(){
         Long mailingId = 1L;
         Mailing mailing = new Mailing();
         mailing.setStatus(Mailing.Status.DELIVERED);
+        mailing.setMailingId(mailingId);
         Mockito.when(mailingService.getById(mailingId)).thenReturn(mailing);
         var actualException = assertThrows(MailingAlreadyDeliveredException.class, ()->movementService.create(mailingId, 1242L));
         assertEquals(HttpStatus.BAD_REQUEST, actualException.getHttpStatus());
         Mockito.verify(mailingService, Mockito.only()).getById(mailingId);
+        Mockito.verify(movementRepository, Mockito.never()).save(Mockito.any());
     }
     @Test
     @DisplayName("create movement when mailing does not exist")
@@ -49,6 +49,32 @@ public class MovementServiceTest {
         Mockito.when(mailingService.getById(mailingId)).then((invocationOnMock -> {
             throw new MailingNotFoundException("Mailing with id + " + mailingId + " not found!", HttpStatus.BAD_REQUEST);
         }));
+        var actualException = assertThrows(MailingNotFoundException.class, ()->movementService.create(mailingId, 1243L));
+        assertEquals(HttpStatus.BAD_REQUEST, actualException.getHttpStatus());
         Mockito.verify(mailingService, Mockito.only()).getById(mailingId);
+        Mockito.verify(movementRepository, Mockito.never()).save(Mockito.any());
+    }
+    @Test
+    @DisplayName("create movement when postal office does not exists")
+    public void createMovementWhenPostalOfficeDoesNotExist(){
+        Long mailingId = 1L;
+        Long postalOfficeIndex = 234234L;
+        Mailing mailing = new Mailing();
+        mailing.setMailingId(mailingId);
+        mailing.setStatus(Mailing.Status.IN_THE_DESTINATION);
+        PostalOffice postalOffice = new PostalOffice();
+        postalOffice.setIndex(postalOfficeIndex);
+        Receiver receiver = new Receiver();
+        receiver.setPostalOffice(postalOffice);
+        mailing.setReceiver(receiver);
+        Mockito.when(postalOfficeService.getById(postalOfficeIndex)).then((invocationOnMock)->{
+           throw new PostalOfficeNotFoundException("Postal office with index " +postalOfficeIndex +" not found!", HttpStatus.BAD_REQUEST);
+        });
+        Mockito.when(mailingService.getById(mailingId)).thenReturn(mailing);
+        var actualException = assertThrows(PostalOfficeNotFoundException.class, ()-> movementService.create(mailingId, postalOfficeIndex));
+        assertEquals(HttpStatus.BAD_REQUEST, actualException.getHttpStatus());
+        Mockito.verify(mailingService, Mockito.times(1)).getById(mailingId);
+        Mockito.verify(postalOfficeService, Mockito.times(1)).getById(postalOfficeIndex);
+        Mockito.verify(movementRepository, Mockito.never()).save(Mockito.any());
     }
 }
