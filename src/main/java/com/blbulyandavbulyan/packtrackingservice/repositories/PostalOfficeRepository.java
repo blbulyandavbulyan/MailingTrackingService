87a8cd683173f149772fde181b6bae5f13977a73
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-21 17:22:48 +0400

    added overriding findById in PostalOfficeRepository

--- src/main/java/com/blbulyandavbulyan/packtrackingservice/repositories/PostalOfficeRepository.java
+++ src/main/java/com/blbulyandavbulyan/packtrackingservice/repositories/PostalOfficeRepository.java
@@ -1,7 +1,16 @@
 package com.blbulyandavbulyan.packtrackingservice.repositories;
 
 import com.blbulyandavbulyan.packtrackingservice.entities.PostalOffice;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.lang.NonNull;
+
+import java.util.Optional;
 
 public interface PostalOfficeRepository extends JpaRepository<PostalOffice, Long> {
+    @Override
+    @NonNull
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<PostalOffice> findById(@NonNull Long id);
 }
