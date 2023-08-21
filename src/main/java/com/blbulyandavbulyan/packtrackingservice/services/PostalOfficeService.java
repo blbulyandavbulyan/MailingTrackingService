07e2dc43ef56857a3248351f3d4d8151fcb22e80
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-21 19:32:57 +0400

    added method getById in PostalOfficeService

--- src/main/java/com/blbulyandavbulyan/packtrackingservice/services/PostalOfficeService.java
+++ src/main/java/com/blbulyandavbulyan/packtrackingservice/services/PostalOfficeService.java
@@ -1,8 +1,10 @@
 package com.blbulyandavbulyan.packtrackingservice.services;
 
 import com.blbulyandavbulyan.packtrackingservice.entities.PostalOffice;
+import com.blbulyandavbulyan.packtrackingservice.exceptions.PostalOfficeNotFoundException;
 import com.blbulyandavbulyan.packtrackingservice.repositories.PostalOfficeRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,5 +17,9 @@ public class PostalOfficeService {
 
     public boolean existByIndex(Long index) {
         return postalOfficeRepository.existsById(index);
+    }
+
+    public PostalOffice getById(Long postalOfficeIndex) {
+        return postalOfficeRepository.findById(postalOfficeIndex).orElseThrow(()->new PostalOfficeNotFoundException("Postal office with index + " + postalOfficeIndex + " not found!", HttpStatus.BAD_REQUEST));
     }
 }
