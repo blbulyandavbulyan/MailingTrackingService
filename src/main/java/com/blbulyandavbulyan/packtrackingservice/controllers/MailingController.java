3f681d04cce92bade3b2c61dc26f4dfce887a965
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-21 12:55:45 +0400

    added MovementService in MailingController, added closing last movement in setDeliveredStatus

--- src/main/java/com/blbulyandavbulyan/packtrackingservice/controllers/MailingController.java
+++ src/main/java/com/blbulyandavbulyan/packtrackingservice/controllers/MailingController.java
@@ -3,8 +3,10 @@ package com.blbulyandavbulyan.packtrackingservice.controllers;
 import com.blbulyandavbulyan.packtrackingservice.dtos.MailingDTO;
 import com.blbulyandavbulyan.packtrackingservice.dtos.MailingInfoDTO;
 import com.blbulyandavbulyan.packtrackingservice.services.MailingService;
+import com.blbulyandavbulyan.packtrackingservice.services.MovementService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class MailingController {
     private MailingService mailingService;
-
+    private MovementService movementService;
     @GetMapping("/{mailingId}")
     public MailingInfoDTO getInfo(@PathVariable("mailingId") Long mailingId){
         return mailingService.getInfo(mailingId);
@@ -22,8 +24,10 @@ public class MailingController {
     public void create(@RequestBody MailingDTO mailingDTO){
         mailingService.create(mailingDTO);
     }
+    @Transactional
     @PatchMapping("/{mailingId}/status/delivered")
     public void setDeliveredStatus(@PathVariable Long mailingId){
         mailingService.setDeliveredStatus(mailingId);
+        movementService.closeLastMovement(mailingId);
     }
 }
