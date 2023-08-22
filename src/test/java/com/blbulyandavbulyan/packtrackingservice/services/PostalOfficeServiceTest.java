8b69a0978b102ea24fbdcf1c3c84a7bc3dad4425
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-22 13:54:34 +0400

    added tests for getById method in PostalOfficeServiceTest

--- src/test/java/com/blbulyandavbulyan/packtrackingservice/services/PostalOfficeServiceTest.java
+++ src/test/java/com/blbulyandavbulyan/packtrackingservice/services/PostalOfficeServiceTest.java
@@ -1,6 +1,7 @@
 package com.blbulyandavbulyan.packtrackingservice.services;
 
 import com.blbulyandavbulyan.packtrackingservice.entities.PostalOffice;
+import com.blbulyandavbulyan.packtrackingservice.exceptions.PostalOfficeNotFoundException;
 import com.blbulyandavbulyan.packtrackingservice.repositories.PostalOfficeRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -42,5 +46,25 @@ public class PostalOfficeServiceTest {
         Mockito.when(postalOfficeRepository.getReferenceById(postalOfficeIndex)).thenReturn(expected);
         PostalOffice actual = underTest.getReferenceById(postalOfficeIndex);
         assertSame(expected, actual);
+    }
+    @Test
+    @DisplayName("get by id when postal office exists")
+    public void getByIdWhenExistsTest(){
+        Long postalOfficeIndex = 1L;
+        PostalOffice expected = new PostalOffice();
+        expected.setIndex(postalOfficeIndex);
+        Mockito.when(postalOfficeRepository.findById(postalOfficeIndex)).thenReturn(Optional.of(expected));
+        PostalOffice actual = underTest.getById(postalOfficeIndex);
+        assertSame(expected, actual);
+        Mockito.verify(postalOfficeRepository, Mockito.only()).findById(postalOfficeIndex);
+    }
+    @Test
+    @DisplayName("get by id when postal office doesn't exist")
+    public void getByIdWhenDoesNotExist(){
+        Long postalOfficeIndex = 1L;
+        Mockito.when(postalOfficeRepository.findById(postalOfficeIndex)).thenReturn(Optional.empty());
+        var actualException = assertThrows(PostalOfficeNotFoundException.class, ()->underTest.getById(postalOfficeIndex));
+        assertEquals(HttpStatus.BAD_REQUEST, actualException.getHttpStatus());
+        Mockito.verify(postalOfficeRepository, Mockito.only()).findById(postalOfficeIndex);
     }
 }
